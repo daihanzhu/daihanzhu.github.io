1c7350a1b4692c8e20edcf9cad1855ab64f79b1d
Matthew Walinga <matthew.walinga@gmail.com>
2018-08-16 22:27:22 -0400

    Make footer links clickable

--- my-app/src/Footer.jsx
+++ my-app/src/Footer.jsx
@@ -8,9 +8,14 @@ class Footer extends Component {
         <div className="footer-content">
           <div className="footer-text">
             <h1>Looking for more?</h1>
-            <p>LinkedIn / Github / Dribble / Instagram / Medium / daihanzhu@gmail.com </p>
+            <p><a href="https://www.linkedin.com/in/daihan/" target="_blank">LinkedIn</a> /
+                <a href="https://github.com/daihanzhu" target="_blank"> Github</a> /
+                <a href="https://dribbble.com/daihan" target="_blank"> Dribble</a> /
+                <a href="https://www.instagram.com/dianezhu/" target="_blank"> Instagram</a> /
+                <a href="https://medium.com/@daihan" target="_blank"> Medium</a> /
+                <a href="mailto:daihanzhu@gmail.com" target="_blank"> daihanzhu@gmail.com </a></p>
             <hr className="line"></hr>
-            <p>Made by Walinga & Daihan with &lt;3 in 2018</p>
+            <p>Made by <a href="https://walinga.github.io" target="_blank">Walinga</a> & Daihan with &lt;3 in 2018</p>
           </div>
           <div className="footer-image-wrapper">
             <img src={require("./images/cat-with-plant.png")} alt="Cat with plant" className="footer-image"></img>
