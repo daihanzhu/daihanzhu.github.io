2aa9a2d6b2f42c0475f0e72921194b34a6f34ab3
Matthew Walinga <matthew.walinga@gmail.com>
2018-09-05 17:57:28 -0400

    Start adding RBCX content

--- my-app/src/TileViews/EmptyState.jsx
+++ my-app/src/TileViews/EmptyState.jsx
@@ -11,9 +11,8 @@ class EmptyState extends Component {
           <div className="content-title">
             <h1>Empty State Illustration</h1>
           </div>
-          <p className="empty-state-desc">OpenText Content Server is a cloud-based enterprise storage software trusted by millions of users. While it is primarily a desktop tool, a mobile app was developed to make content management more easily accessible and user-friendly. As opposed to most other apps, the goal here is not user retention, but to enable them to get work done fast. Also, who enterprise software has to be boring? Therefore, I was asked by OpenText to re-imaging the empty states of the mobile app in order to delight, inform, and provide a positive user-experience.
-          <br/>
-
+          <p className="content-desc">OpenText Content Server is a cloud-based enterprise storage software trusted by millions of users. While it is primarily a desktop tool, a mobile app was developed to make content management more easily accessible and user-friendly. As opposed to most other apps, the goal here is not user retention, but to enable them to get work done fast. Also, who enterprise software has to be boring? Therefore, I was asked by OpenText to re-imaging the empty states of the mobile app in order to delight, inform, and provide a positive user-experience.
+          <br/><br/>
           This project roughy spanned over three weeks to completion. With guidance from the OpenText Visual Design Team.</p>
           <img src={require('../images/emptyState/original.png')} alt='Original State' className="empty-state-img es-img1"></img>
           <h2 className="original-state-caption">The problem</h2>
@@ -23,13 +22,13 @@ class EmptyState extends Component {
           </p>
           <h2>The goal</h2>
           <p>The first step is to establish the goals I’d like to achieve with this project. While I was given the freedom and creativity to explore, I wanted to set some personal goals to measure success later on.
-            <br/>
+            <br/><br/>
             Make the in-app experience more friendly and personable
-            <br/>
+            <br/><br/>
             Turn a moment of loss or frustration into something funny and hopefully relatable
-            <br/>
+            <br/><br/>
             Can help guide new users to discover a new feature, act as an extension to the user onboarding
-            <br/>
+            <br/><br/>
             Begin the establishment of illustration guideline as there are currently no illustrations within the product
           </p>
           <div className="clear-float"></div>
@@ -42,40 +41,48 @@ class EmptyState extends Component {
           <p>Since there aren't many previous illustrations to go off of, I looked elsewhere. I gathered as many inspirations from other similar products in order to find a good starting point. While there are established illustraton standards from Marketing, product illustrations don’t nessasarily speak the same language. Marketing illustration are often used to highlight the features, while product, illustration usually find refuge sparingly and assit the user to convey meaningful information.</p>
           <h2>Shapes and colour</h2>
           <p>With that being said, I began experiementing with shapes and colour combinations. Working off of a dark gradient background, I stayed away from using contrasting colors and played with gradient and opacity instead. Even though the current background creates a form of constrain, I ensured the flexibility of the colors can be reused against other backgrounds.</p>
-          <h2>Sketches</h2>
-          <p>Next, I turned to pen and paper and sketched out possible scenes and charactors. Sketching has always been my favourite part of the design process. I went through pages of rough drawings without worring too much about how it looks. My goal here is to take each senarios and come up with as many ways as I can use illustrations to convey the meaning.</p>
           <div className="clear-float"></div>
+          <div className="par-block">
+            <h2>Sketches</h2>
+            <p>Next, I turned to pen and paper and sketched out possible scenes and charactors. Sketching has always been my favourite part of the design process. I went through pages of rough drawings without worring too much about how it looks. My goal here is to take each senarios and come up with as many ways as I can use illustrations to convey the meaning.</p>
+          </div>
         </div>
         <img src={require('../images/emptyState/mid-header.png')} alt='Mid Header' className="mid-header"></img>
         <div className="content-wrapper">
-          <h2>Actually doing it</h2>
-          <p>When I brough the sketches into illustrator, I got some valuable feedback on which sketch might work the best with each screen. I took some elements of previously created illustrations from product marketing since it worked really well in making the drawings come to life. This was also helpful since consistency and making the drawings feel like they belong in a set was something I had intentions for.
+          <div className="par-block">
+            <h2>Actually doing it</h2>
+            <p>When I brough the sketches into illustrator, I got some valuable feedback on which sketch might work the best with each screen. I took some elements of previously created illustrations from product marketing since it worked really well in making the drawings come to life. This was also helpful since consistency and making the drawings feel like they belong in a set was something I had intentions for.
 
-          Oh, and yes, I had to sneak a cat in there somehow. </p>
-          <h2>Getting feedback</h2>
-          <p>
-          Along the way, I have consistently bothered a few of my co-workers for feedback. They are all incrediabily helpful in making sure I’m going in the right direction by asking me if I have thought of doing it another way, giving suggestions, and sending me resources that might help.
+            Oh, and yes, I had to sneak a cat in there somehow. </p>
+            <h2>Getting feedback</h2>
+            <p>
+            Along the way, I have consistently bothered a few of my co-workers for feedback. They are all incrediabily helpful in making sure I’m going in the right direction by asking me if I have thought of doing it another way, giving suggestions, and sending me resources that might help.
 
-          One thing I’m still trying to work on is stop packaging up my unfinished work up nicely just for getting feedback. At first, it was quite daunting to show my messey half-finished art-boards to the people around me. But I quickly realized that everyone is always asking and providing feedback on their work in process regardless of where they are at, and yes, we all have the disorganized artboards to untangle at some point.</p>
+            One thing I’m still trying to work on is stop packaging up my unfinished work up nicely just for getting feedback. At first, it was quite daunting to show my messey half-finished art-boards to the people around me. But I quickly realized that everyone is always asking and providing feedback on their work in process regardless of where they are at, and yes, we all have the disorganized artboards to untangle at some point.</p>
+          </div>
           <img src={require('../images/emptyState/12-phones.png')} alt='Empty States' className="es-img3"></img>
-          <h2>Looking back</h2>
-          <p>I’m overall very happy with how these turned out. I learned throughout this process is that empty states are no joke. They are some of the first things a new user see when they first download the app. They are also things people see when something didn’t go as planned, but can help turn a moment of frustration to delight.
-            <br/>
-            I’ve came across many great case studies about their process behind creating great empty states, they have all helped me in one way or another in creating my own.
-            <br/>
-            Empty States? More like You-Have-No-Idea-How-Much-Work-Goes-Into-Those States, amirite?? By Meg Robichaud
-            https://ux.shopify.com/empty-states-more-like-you-have-no-idea-how-much-work-goes-into-those-states-amirite-e0102f58b64e
-            <br/>
-            Illustrating a more human brand
-            https://medium.com/dropbox-design/illustrating-a-more-human-brand-part-1-e1cfe3c28d9
-          </p>
-          <h2>The Final Set</h2>
-          <p>The background color got a refresher as well.</p>
+          <div className="par-block">
+            <h2>Looking back</h2>
+            <p>I’m overall very happy with how these turned out. I learned throughout this process is that empty states are no joke. They are some of the first things a new user see when they first download the app. They are also things people see when something didn’t go as planned, but can help turn a moment of frustration to delight.
+              <br/>
+              I’ve came across many great case studies about their process behind creating great empty states, they have all helped me in one way or another in creating my own.
+              <br/>
+              Empty States? More like You-Have-No-Idea-How-Much-Work-Goes-Into-Those States, amirite?? By Meg Robichaud
+              https://ux.shopify.com/empty-states-more-like-you-have-no-idea-how-much-work-goes-into-those-states-amirite-e0102f58b64e
+              <br/>
+              Illustrating a more human brand
+              https://medium.com/dropbox-design/illustrating-a-more-human-brand-part-1-e1cfe3c28d9
+            </p>
+            <h2>The Final Set</h2>
+            <p>The background color got a refresher as well.</p>
+          </div>
         </div>
         <img src={require('../images/emptyState/emptyall.png')} alt='More Empty States' className="mid-header"></img>
         <div className="content-wrapper">
-          <h2>Just getting started</h2>
-          <p>The project isn’t over yet! Since recieving the positive feebacks from mobile, I was asked to create more to go into the desktop app. Some very small adjestments were made to fully optimize it for the different color backgrounds. Here’s what they look like.  </p>
+          <div className="par-block">
+            <h2>Just getting started</h2>
+            <p>The project isn’t over yet! Since recieving the positive feebacks from mobile, I was asked to create more to go into the desktop app. Some very small adjestments were made to fully optimize it for the different color backgrounds. Here’s what they look like.  </p>
+          </div>
         </div>
         <img src={require('../images/emptyState/coffee.png')} alt='Empty States' className="es-img3"></img>
         <div className="es-shape-container">
--- my-app/src/TileViews/RBCX.jsx
+++ my-app/src/TileViews/RBCX.jsx
@@ -11,33 +11,36 @@ class EmptyState extends Component {
           <div className="content-title">
             <h1>RBCX - Powered by Venture Inside RBC</h1>
           </div>
-          <p className="rbcx-desc">I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
-            I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
-            I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:</p>
-          <img src={require('../images/emptyState/original.png')} alt='Original State' className="rbcx-img rbcx-img1"></img>
-          <h2> The Rebrand</h2>
-          <p>Here's a bunch of text about the image on the left</p>
-          <h2> The goal</h2>
-          <p>Here's my goal blah blah blah</p>
+          <p className="content-desc">In summer 2017, I joined the Intrapreneurship team (Venture Inside) as their first ever design intern at the RBC Royal Bank of Canada. Our mission was to challenge and enable RBC employees around the globe to solve important problems, develop product thinking, and innovate much like entrepreneurs, but with the resource of a multinational company.
+            <br/><br/>
+            RBCx is a startup accelerator program powered by Venture Inside that puts employees through an intensive 10-week program to solve big, disruptive problems. From my very first day, I was thrown right into the action and worked with the venture teams to develop their ideas into a prototype. From there, I worked closely with the managing team to re-design the branding for both Venture Inside and RBCx, as well as multiple resources on topics such as Design Thinking and problem pitching.</p>
+          <img src={require('../images/rbcx/our-brand.png')} alt='Original State' className="rbcx-img rbcx-img2"></img>
+          <h2>What is our brand?</h2>
+          <p>When I first started, both Venture Inside and RBCx were in their early days, much like a startup in itself. Our biggest goal was to spread the words about our exsistance to gauge interest internally. So, at each townhall and expo, we put the thought into creating experiences that is both interactive and unique to simulate an entrepreneurial mindset.
+            <br/><br/>
+            Such as these trading cards I designed, they helped introduce and break down the concept of problem solving through Design Thinking.
+            <br/><br/>
+            This led to many brainstorming sesh on what we want to convey through our brand. Who are we and what do we do? Slowly, the picture is starting to take shape. </p>
           <div className="clear-float"></div>
-          <img src={require('../images/emptyState/inspiration.png')} alt='My inspiration' className="rbcx-img rbcx-img2"></img>
-          <h2> The problem</h2>
-          <p>Here's a bunch of text about the image on the left</p>
-          <h2> The goal</h2>
-          <p>Here's my goal blah blah blah</p>
-          <img src={require('../images/emptyState/shapes.png')} alt='Shapes and Colours' className="rbcx-img rbcx-img2"></img>
-          <div className="clear-float"></div>
+          <img src={require('../images/rbcx/rbcx-logo.png')} alt='My inspiration' className="rbcx-img3"></img>
+          <div className="par-block">
+            <h2>Gather inspiration</h2>
+            <p>First, I wanted to gather inspirations from other similar products and see how they approach empty states. Sort of like building a moodboard with research to build a good foundation to work on.</p>
+            <h2>Shapes and colour</h2>
+            <p>Next, I began experiementing with shapes and colour combinations. One of the constrains of the illustrations is that they have to appear against a dark gradient background. There might also be the possibility of alternative usage for the illustrations so they shoud be flexible and reuseable. I played with different combinations of shapes and lines and decided to keep the illustrations simple and use solid shapes and lines with rounded end caps as a starting point.</p>
+          </div>
         </div>
-        <img src={require('../images/emptyState/mid-header.png')} alt='Mid Header' className="mid-header"></img>
+        <img src={require('../images/rbcx/rbc-dots.png')} alt='Mid Header' className="mid-header rbc-dots"></img>
+        {/* TODO: Beyond this point is empty state content */}
         <div className="content-wrapper">
           <h2>Actually doing it</h2>
-          <p className="rbcx-desc">I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
-            I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
-            I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:</p>
+          <p className="rbcx-desc">When I brough the sketches into illustrator, I got some valuable feedback on which sketch might work the best with each screen. I took some elements of previously created illustrations from product marketing since it worked really well in making the drawings come to life. This was also helpful since consistency and making the drawings feel like they belong in a set was something I had intentions for.
+          <br/><br/>
+          Oh, and yes, I tried really hard to sneak a cat in there somehow. </p>
           <h2>Getting feedback</h2>
-          <p className="rbcx-desc">I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
-            I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
-            I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:</p>
+          <p className="rbcx-desc">Along the way, I have consistently bothered a few of my co-workers for feedback. They are all incrediabily helpful in making sure I’m going in the right direction by asking me if I have thought of doing it another way, giving suggestions, and sending me resources that might help.
+          <br/><br/>
+          One thing I’m still trying to work on is stop packaging up my unfinished work up nicely just for getting feedback. At first, it was quite daunting to show my messey half-finished art-boards to the people around me. But I quickly realized that everyone is always asking and providing feedback on their work in process regardless of where they are at, and yes, we all have the disorganized artboards to untangle at some point.</p>
           <img src={require('../images/emptyState/12-phones.png')} alt='Empty States' className="rbcx-img3"></img>
           <h2>Looking back</h2>
           <p className="rbcx-desc">I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
