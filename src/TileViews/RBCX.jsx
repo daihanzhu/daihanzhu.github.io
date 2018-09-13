6a2e78cdb353283b34a3c1a131bcf4776018ff6d
Matthew Walinga <matthew.walinga@gmail.com>
2018-09-13 00:52:48 -0400

    Add content, styling, and stuff for RBCX

--- src/TileViews/RBCX.jsx
+++ src/TileViews/RBCX.jsx
@@ -3,7 +3,44 @@ import './RBCX.css';
 import '../ContentWrapper.css'
 
 class EmptyState extends Component {
+  constructor(props) {
+    super(props)
+    this.updateDimensions = this.updateDimensions.bind(this);
+
+    this.state = {
+      width: 0
+    }
+  }
+
+  updateDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
+
+  componentWillMount() {
+    this.updateDimensions();
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+    this.updateDimensions()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
   render() {
+    console.log(this.state.width );
+    const designThinkingImages = this.state.width >= 650 ?
+      (<div><img src={require('../images/rbcx/backpacker.png')} alt="Design Thinking Book" className="backpacker"></img>
+      <img src={require('../images/rbcx/dt-page4.png')} alt="Design Thinking Page 4" className="open-book"></img>
+      <img src={require('../images/rbcx/dt-cover.png')} alt="Design Thinking Cover" className="backpacker"></img>
+      <img src={require('../images/rbcx/dt-page3.png')} alt="Design Thinking Page 3" className="open-book"></img></div>) :
+      (<div><img src={require('../images/rbcx/backpacker.png')} alt="Design Thinking Book" className="backpacker"></img>
+      <img src={require('../images/rbcx/dt-cover.png')} alt="Design Thinking Cover" className="backpacker"></img>
+      <img src={require('../images/rbcx/dt-page3.png')} alt="Design Thinking Page 3" className="open-book"></img>
+      <img src={require('../images/rbcx/dt-page4.png')} alt="Design Thinking Page 4" className="open-book"></img></div>);
+
     return (
       <div className="rbcx">
         <div className="rbcx-header content-header"></div>
@@ -11,52 +48,69 @@ class EmptyState extends Component {
           <div className="content-title">
             <h1>RBCX - Powered by Venture Inside RBC</h1>
           </div>
-          <p className="content-desc">In summer 2017, I joined the Intrapreneurship team (Venture Inside) as their first ever design intern at the RBC Royal Bank of Canada. Our mission was to challenge and enable RBC employees around the globe to solve important problems, develop product thinking, and innovate much like entrepreneurs, but with the resource of a multinational company.
+          <p className="content-desc rbcx-desc">In summer 2017, I joined the Intrapreneurship team (Venture Inside) as their first ever design intern at the RBC Royal Bank of Canada. Our mission was to challenge and enable RBC employees around the globe to solve important problems, develop product thinking, and innovate much like entrepreneurs, but with the resource of a multinational company.
             <br/><br/>
             RBCx is a startup accelerator program powered by Venture Inside that puts employees through an intensive 10-week program to solve big, disruptive problems. From my very first day, I was thrown right into the action and worked with the venture teams to develop their ideas into a prototype. From there, I worked closely with the managing team to re-design the branding for both Venture Inside and RBCx, as well as multiple resources on topics such as Design Thinking and problem pitching.</p>
-          <img src={require('../images/rbcx/our-brand.png')} alt='Original State' className="rbcx-img rbcx-img2"></img>
-          <h2>What is our brand?</h2>
-          <p>When I first started, both Venture Inside and RBCx were in their early days, much like a startup in itself. Our biggest goal was to spread the words about our exsistance to gauge interest internally. So, at each townhall and expo, we put the thought into creating experiences that is both interactive and unique to simulate an entrepreneurial mindset.
+          <img src={require('../images/rbcx/venture-inside.png')} alt='Venture Inside' className="rbcx-img rbcx-img2"></img>
+          <div className="side-caption">
+            <h2 className="venture-caption">Discovery</h2>
+            <p>Designing a logo goes beyound the pixels that appear on an artboard. The first step is to gather a holistic view of the representation of the brand to the point that every detail has been considered.
             <br/><br/>
-            Such as these trading cards I designed, they helped introduce and break down the concept of problem solving through Design Thinking.
+            The team and I went through countless sketches, feedbacks, and whiteboarding sessions to what it eventually become. A logo that is flexible, recognizable, and representative of our slick yet candid personality.
             <br/><br/>
-            This led to many brainstorming sesh on what we want to convey through our brand. Who are we and what do we do? Slowly, the picture is starting to take shape. </p>
+            Branding should be fluid. It can be build upon and taken apart. They have an attitude and can possese personality of their own, and that’s what makes it so much fun. </p>
+          </div>
           <div className="clear-float"></div>
-          <img src={require('../images/rbcx/rbcx-logo.png')} alt='My inspiration' className="rbcx-img3"></img>
           <div className="par-block">
-            <h2>Gather inspiration</h2>
-            <p>First, I wanted to gather inspirations from other similar products and see how they approach empty states. Sort of like building a moodboard with research to build a good foundation to work on.</p>
-            <h2>Shapes and colour</h2>
-            <p>Next, I began experiementing with shapes and colour combinations. One of the constrains of the illustrations is that they have to appear against a dark gradient background. There might also be the possibility of alternative usage for the illustrations so they shoud be flexible and reuseable. I played with different combinations of shapes and lines and decided to keep the illustrations simple and use solid shapes and lines with rounded end caps as a starting point.</p>
+            <h2>Iterating</h2>
+            <p>Now that Venture Inside has its own shiny identity, time for RBCx to get a refresher as well. When I joined, RBCx was just wrapping up its first cohort and gearing up for the next one, so a large part of the process is iterating and taking it to the next level. Starting with the branding.</p>
           </div>
         </div>
-        <img src={require('../images/rbcx/rbc-dots.png')} alt='Mid Header' className="mid-header rbc-dots"></img>
-        {/* TODO: Beyond this point is empty state content */}
-        <div className="content-wrapper">
-          <h2>Actually doing it</h2>
-          <p className="rbcx-desc">When I brough the sketches into illustrator, I got some valuable feedback on which sketch might work the best with each screen. I took some elements of previously created illustrations from product marketing since it worked really well in making the drawings come to life. This was also helpful since consistency and making the drawings feel like they belong in a set was something I had intentions for.
-          <br/><br/>
-          Oh, and yes, I tried really hard to sneak a cat in there somehow. </p>
-          <h2>Getting feedback</h2>
-          <p className="rbcx-desc">Along the way, I have consistently bothered a few of my co-workers for feedback. They are all incrediabily helpful in making sure I’m going in the right direction by asking me if I have thought of doing it another way, giving suggestions, and sending me resources that might help.
-          <br/><br/>
-          One thing I’m still trying to work on is stop packaging up my unfinished work up nicely just for getting feedback. At first, it was quite daunting to show my messey half-finished art-boards to the people around me. But I quickly realized that everyone is always asking and providing feedback on their work in process regardless of where they are at, and yes, we all have the disorganized artboards to untangle at some point.</p>
-          <img src={require('../images/emptyState/12-phones.png')} alt='Empty States' className="rbcx-img3"></img>
-          <h2>Looking back</h2>
-          <p className="rbcx-desc">I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
-            I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
-            I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:</p>
-          <h2>The Final Set</h2>
-          <p className="rbcx-desc">I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
-            I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:
-            I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:</p>
+        <img src={require('../images/rbcx/rbcx-logo.png')} alt='My inspiration' className="rbcx-img3"></img>
+        <div className="poster-container">
+          <div className="rbcx-img4-cont">
+            <img src={require('../images/rbcx/rbcx-poster.png')} alt='RBCX Poster' className="rbcx-img4"></img>
+          </div>
+          <div className="poster-desc">
+            <div className="poster-desc-inner">
+              <h2>Isometric Illustration</h2>
+              <p>I designed and illustrated this poster to promote RBCx. I was inspired by an ad I saw in the subway station in this isometric style and became hooked. At first, I intended to just play around in illustrator and maybe make it into a Snapchat geofilter or something, but 6 hours later, this happend.
+              <br/><br/>
+              I call this the idea factory. You can follow the “ideas” out of office building on the left and see it magically turn into bananas, which then goes to power the lights. Of course, the bananas get recycled back into the system because no idea is a rotten idea.
+              <br/><br/>
+              My proudest moment was when this poster was plastered on the projector during a townhall at the Art Gallary of Ontario, maybe it belongs there or something. </p>
+            </div>
+          </div>
         </div>
-        <img src={require('../images/emptyState/emptyall.png')} alt='More Empty States' className="mid-header"></img>
-        <div className="content-wrapper">
-          <h2>Looking back</h2>
-          <p className="rbcx-desc">I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS: I make empty states :monkaS:</p>
+        <div className="rbc-wrapper-a">
+          <img src={require('../images/rbcx/rbc-dots.png')} alt='' className="rbc-dots"></img>
+          <img src={require('../images/rbcx/founder.png')} alt='Founder experience book' className="founder-img"></img>
+          <div className="pink-box"></div>
+          <div className="center-box playbook-box">
+            <h2>RBCx Experience Playbook</h2>
+            <p>A series of playbooks was designed to equipt founders, consultants, and advisors with the information they needed from the very first day to find success throughout the 10 weeks.</p>
+          </div>
         </div>
-        <img src={require('../images/emptyState/coffee.png')} alt='Empty States' className="rbcx-img3"></img>
+        {designThinkingImages}
+        <div className="center-box">
+          <h2>Design Thinking Work Book</h2>
+          <p>A series of playbooks was designed to equipt founders, consultants, and advisors with the information they needed from the very first day to find success throughout the 10 weeks.</p>
+        </div>
+        <div className="content-wrapper mid-content-wrapper">
+          <img src={require('../images/rbcx/our-brand.png')} alt="Our Brand" className="our-brand"></img>
+          <img src={require('../images/rbcx/hand-with-cards.png')} alt="Hand with cards" className="hand-cards"></img>
+          <div className="par-block">
+            <h2>Insight 1: people like information in bite sizes</h2>
+            <p>This set of trading cards I designed were passed out at townhalls and expos to introduce and break down the concept of problem solving through Design Thinking. They were a huge hit and I especailly loved seeing people sifting through the pile trying to collect them all, as if they were pokemon cards or something.</p>
+            <h2>Insight 2: innovation doesn’t have to be complicated</h2>
+            <p>Mid way through the summer, I wanted to give community engagement a try. So I band together a group of interns and brainstormed ways we can get everyone excited about intrapreneurship. We knew it had to be interactive, fun, and accssible, so a pop-up shop quickly came to mind.
+            <br/><br/>
+            In the past, we’ve used “pipe cleaner prototyping” as an actitivity to get people thinking on their feet and use their hands to make something tangible, so along with that, we came up with a series of challenges that draws attention to teamwork, problem solving, and getting you outside of your comfort zone. Some of these challenges include the classic egg drop, spaghetti tower, and building a Rube Goldburg Machine. Oh and VR games, because VR is cool. I wrote more about it
+            <a href="https://medium.com/@daihan/a-beginners-guide-to-innovation-b78be55ceda9" target="_blank"> here</a>.
+            <br/><br/>
+            We had such a great turn out that we had to tell people to quite down at some point because they got too into the challenges.</p>
+          </div>
+        </div>
       </div>
     );
   }
