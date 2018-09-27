5b85e392e070060807377eb521d8e2f75dc3ab9b
Matthew Walinga <matthew.walinga@gmail.com>
2018-09-26 23:40:59 -0400

    Add google analytics

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import Main from './Main';
 import registerServiceWorker from './registerServiceWorker';
 
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-106973282-1');
+ReactGA.pageview('/homepage');
+
 ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
