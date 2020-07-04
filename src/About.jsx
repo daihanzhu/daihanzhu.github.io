f3b66bf0888b49777e2601ff5a526b1a7707ef37
Daihan Zhu <daihanzhu@gmail.com>
2020-07-03 21:59:18 -0600

    Font updates

--- src/About.jsx
+++ src/About.jsx
@@ -37,36 +37,41 @@ class About extends Component {
             <div className="daihan-who"></div>
           </div>
           <div className="daihan-caption">
-            <h1>Who I am</h1>
-            <br/>
-            <p className="daihan-desc">Aside from being a designer (I hope you got that by now), I’m an occasional cat-sitter, writer, and entrepreneur. In the past, I’ve been a book shelver, curler, and guilty of over-filtering my Instagram photos. In the future, I would like to grow my own avocados.<br/>
-            <br/>
-            For the time being, I make a lot of mistakes and I’m overly curious about everything around me.<br/>
+            <h1>Let’s be friends</h1>
+            
+            <p className="daihan-desc">I’ll start. My name is Daihan, and I design software people like to use.
+<br/><br/>
+In the past, I’ve lived on 3 continents (North America, Europe, Asia), been an alphabetizer of books, 4th place winning curler, and guilty of over-filtering my online self. 
+<br/><br/>
+In the future, I plan on illustrating a children’s book, grow an avocado tree, and ride horses into the sunset. 
+<br/><br/>
+For the time being, I make a lot of mistakes and I’m overly curious about everything around me.
+            
           </p>
           </div>
           <div className="clear-float"></div>
           {/*{blocks}*/}
           <div className="about-text-block">
-            <h1 className="about-section-header">Meet my cats</h1> 
+            <h1 className="about-section-header">You have to meet my cats</h1> 
             <div className="list-column">
               <ol>
                 <img src={require('./images/Cat1.png')} alt="Black cat" className="cat-pic"></img>
                 <h2>Cat #1</h2>
-                He likes eating
+                A sweetheart deep down. Will work for treats. Often found staring into other people’s homes while perched on the fence. Doesn't like you.
               </ol>
             </div>
             <div className="list-column">
               <ol>
                 <img src={require('./images/Florien.png')} alt="Gray cat" className="cat-pic"></img>
                 <h2>Florien</h2>
-                He likes petting
+                A purr machine! Likes to play fetch but will absolutly judge your throw. Oh, and keep an eye on your shoes, he might've puked in them...
               </ol>
             </div>
             <div className="list-column">
               <ol>
                 <img src={require('./images/SmallCat.png')} alt="Small Gray cat" className="cat-pic"></img>
-                <h2>Small cat</h2>
-                She likes hiding
+                <h2>Smol Cat</h2>
+                A cat? Who really knows. She is buttery soft and very vocal about her needs. If you can’t find her, look in a cupboard. Escape attempts: 3
               </ol>
             </div>
           </div>
