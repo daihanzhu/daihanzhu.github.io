116680550e691ffd88a0d7696c28fbb511306339
walinga <matthew.walinga@gmail.com>
2020-07-10 22:55:37 -0600

    Updated kudos page (pending approval)

--- src/TileViews/Kudos.jsx
+++ src/TileViews/Kudos.jsx
@@ -14,23 +14,22 @@ class Kudos extends Component {
       <div className="kudos">
         <div className="kudos-header content-header">
           <div className="kudos-inlay">
-            <img src={this._fetchImage('screenshot1.png')} alt='Kudos UI' className="kudos-inlay-img"></img>
+            <img src={this._fetchImage('phones.png')} alt='Kudos UI' className="kudos-inlay-img"></img>
             <div className="kudos-title">
               <h3>UI/UX</h3>
               <h1>Giving Thanks</h1>
+              <p className="kudos-desc">Year: <b>2019-present</b> | Role: <b>Sole Product Designer</b> | Tools: <b>Figma</b><br/>
+                <br/>
+                The Kudos platform empowers users to recognize the achievements of their coworkers and earn points that they can spend on gift cards.<br/>
+              </p>
             </div>
           </div>
         </div>
         <div className="kudos-wrapper content-wrapper">
-          <div className="kudos-caption">
-            <h1>What is Kudos?</h1>
-            <br/>
-            <p className="daihan-desc"><b>Kudos</b>. <i>noun</i> : praise and honor received for an achievement.<br/>
-            <br/>
-            The Kudos platform empowers users to recognize the achievements of their coworkers and earn points that they can spend on gift cards.<br/>
-          </p>
-          </div>
+          {/*<div className="kudos-caption">
+        </div>*/}
           <div className="clear-float"></div>
+          <img src={this._fetchImage('kudos-display 1.png')} alt='Kudos UI' className="kudos-big-img"></img>
         </div>
       </div>
 
