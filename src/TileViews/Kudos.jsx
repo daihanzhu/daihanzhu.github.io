de8f4ae7410178e67f50887279218698dc370848
walinga <matthew.walinga@gmail.com>
2020-09-13 19:29:59 -0600

    Add mailto link and some more text

--- src/TileViews/Kudos.jsx
+++ src/TileViews/Kudos.jsx
@@ -20,16 +20,18 @@ class Kudos extends Component {
               <h1>Giving Thanks</h1>
               <p className="kudos-desc">Year: <b>2019-present</b> | Role: <b>Sole Product Designer</b> | Tools: <b>Figma</b><br/>
                 <br/>
-                The Kudos platform empowers users to recognize the achievements of their coworkers and earn points that they can spend on gift cards.<br/>
+                The Kudos platform empowers users to recognize the achievements of their coworkers and earn points that they can spend on gift cards.<br/><br/>
+                I have the exciting opportunity to shape the design of Kudos from the ground up. Kudos is an Enterprise Recognition Platform serving clients in over 80 countries.
               </p>
             </div>
           </div>
         </div>
         <div className="kudos-wrapper content-wrapper">
-          {/*<div className="kudos-caption">
-        </div>*/}
           <div className="clear-float"></div>
           <img src={this._fetchImage('kudos-display 1.png')} alt='Kudos UI' className="kudos-big-img"></img>
+          <div className="kudos-caption">
+            <a href="mailto:daihanzhu@gmail.com" target="_blank" className="kudos-contact"> Get in touch</a> if you’d like to learn more about the design process
+            </div>
         </div>
       </div>
 
