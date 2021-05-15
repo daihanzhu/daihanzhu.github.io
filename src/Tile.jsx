e7f2ec5e21c6e89bf84f5f945fa201b32a900445
walinga <matthew.walinga@gmail.com>
2021-05-14 21:25:00 -0600

    Color-code tile headers

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -14,12 +14,21 @@ class Tile extends Component {
         <div className="overlay-text">{this.props.overlaytext}</div>
     </div> ) : null;
 
+    let headerBgColor = '#FFE6DC';
+    if (this.props.header === 'Brand Design') {
+      headerBgColor = '#DCE6FF';
+    } else if (this.props.header === 'Illustration') {
+      headerBgColor = '#FFF8C2';
+    }
+
+    const headerStyle = {backgroundColor: headerBgColor};
+
     return (
       <div className="tile" onClick={this.handleClick}>
         <img src={this.props.image} alt={this.props.altText} className="tile-bgd"></img>
         <div className="tile-bottom">
           <div className="tile-text-box">
-            <div className="tile-header-text">{this.props.header}</div>
+            <div style={headerStyle} className="tile-header-text">{this.props.header}</div>
             <h2 className="tile-title">{this.props.title}</h2>
             <div className="tile-desc">{this.props.desc}</div>
           </div>
