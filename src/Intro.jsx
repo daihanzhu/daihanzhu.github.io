f79cfbc735621aeba3a75fb19ac8e416ea1246de
walinga <matthew.walinga@gmail.com>
2025-06-07 14:42:10 -0400

    Landing page style updates

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -7,7 +7,7 @@ import Views from './Views.js';
 class Intro extends Component {
   mainContent() {
     return (<div>
-      <h1>Hello, I’m Daihan (she/her)<br/>I’m a product designer and illustrator</h1>
+      <h1>Hello, I’m Daihan <span className="pronouns">(she/her)</span><br/>I’m a product designer and illustrator</h1>
       <p className="daihan-intro">        
         As a designer, I’m on a mission to make people’s digital experiences more delightful, inclusive, and valuable. I think holistically while placing every pixel with purpose. 
         <br/><br/>
